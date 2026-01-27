bda8ce1b4ed16dd7d7815fdfdfb738d94b1f89c7
asica106 <a.sica106@studenti.unisa.it>
2026-01-27 16:50:14 +0100

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,7 @@
     <ul class="nav-links" id="navLinks">
         <?php if (isset($_SESSION['user_id'])): ?>
             <li>
-                <a href="create_thread.php" class="btn-create-thread">
+                <a href="includes/create_thread.php" class="btn-create-thread">
                     <svg width="18" height="18" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
                         <path d="M19 9.375h-8.375V1h-1.25v8.375H1v1.25h8.375V19h1.25v-8.375H19v-1.25Z"></path>
                     </svg>
