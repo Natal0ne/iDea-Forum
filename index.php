6ad09eb696bc410e3c7bbd3f31879c180cc7bf50
Natale Affinita <67345055+Natal0ne@users.noreply.github.com>
2026-02-03 15:36:32 +0100

    Add navbar include and simplify styles

--- index.php
+++ index.php
@@ -6,15 +6,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>iDea</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">    
+    <link rel="stylesheet" href="assets/css/navbar.css">    
 </head>
 <body>
-    <div class="classe">
-        <h1 class="logo bitcount-single">Welcome to iDea</h1>
-    </div>
-
-    <div class="page-content">
-        <p>Bella a tutti ragazzi </p>
-    </div>
+    <?php require_once 'includes/navbar.php' ?>
 </body>
 </html>
