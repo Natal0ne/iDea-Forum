d3579bf19f141e19c3f27681fd2bc558dfcc858b
Natale Affinita <67345055+Natal0ne@users.noreply.github.com>
2026-02-03 15:08:37 +0100

    Start without AI

--- index.php
+++ index.php
@@ -9,22 +9,12 @@
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <?php include "includes/header.php"; ?>
-    <?php include "includes/login.php"; ?>
-    <?php include "includes/register.php"; ?>
-
-    <div class="hero-header">
-        <h1>Welcome to iDea</h1>
+    <div class="classe">
+        <h1 class="logo bitcount-single">Welcome to iDea</h1>
     </div>
 
     <div class="page-content">
         <p>Bella a tutti ragazzi </p>
     </div>
-    
-    <?php include "includes/create_thread.php"; ?>
-
-    <?php include "includes/footer.php"; ?>
-
-    <script src="assets/js/script.js"></script>
 </body>
 </html>
