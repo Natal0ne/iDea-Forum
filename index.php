f6ad96cbae9837421664a7ff7de1d00394ccb058
Rondosvalle <rubenrusso360@proton.me>
2026-02-03 15:43:42 +0100

    Added gradient div

--- index.php
+++ index.php
@@ -2,14 +2,31 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>iDea</title>
-    <link rel="stylesheet" href="assets/css/style.css">    
-    <link rel="stylesheet" href="assets/css/navbar.css">    
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/navbar.css">
 </head>
+
 <body>
     <?php require_once 'includes/navbar.php' ?>
+
+    <div class="gradient-box">
+        <h1>Welcome to iDea</h1>
+    </div>
+    <p>Ciao</p>
+    <p>Ciao</p>
+    <p>Ciao</p>
+    <p>Ciao</p>
+    <p>Ciao</p>
+    <p>Ciao</p>
+    <p>Ciao</p>
+
+    <script src="assets/js/script.js"></script>
+    <script src="assets/js/navbar.js"></script>
 </body>
+
 </html>
