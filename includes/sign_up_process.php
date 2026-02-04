d535b2f844e9aa37279e59fa26d5a5c90775ac87
asica106 <a.sica106@studenti.unisa.it>
2026-02-04 16:44:56 +0100

    Updates in sign_up_process

--- includes/sign_up_process.php
+++ includes/sign_up_process.php
@@ -8,27 +8,33 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $password = $_POST['password'];
 
     $hash = password_hash($password, PASSWORD_DEFAULT);
+    
 
     $conn = connect_db();
+    $stmt_name = "signup_query";
 
     $query = "
         INSERT INTO users (username, email, password_hash)
         VALUES ($1, $2, $3)
     ";
 
-    $result = pg_query_params($conn, $query, [
-        $username,
-        $email,
-        $hash
-    ]);
+     if (!pg_prepare($conn, $stmt_name, $query)) {
+        die("Prepare failed: " . pg_last_error());
+    }
+
+    $result = pg_execute($conn, $stmt_name, array($username, $email, $hash));
 
     pg_close($conn);
 
     if ($result) {
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        $_SESSION['email'] = $user['email'];
+        $_SESSION['role'] = $user['role'];       
         header("Location: /index.php");
         exit;
-    } else {
-        header("Location: /register.php?error=1");
-        exit;
     }
+    $_SESSION['sign_up_error'] = "Invalid username or password.";
+    header(header: "Location: ../index.php");
+    exit;
 }
