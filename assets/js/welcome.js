d2334e53a2644c8a4e513992da33dc9cbfb45f77
Rondosvalle <rubenrusso360@proton.me>
2026-02-04 19:36:20 +0100

    Welcome animation tweaks

--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -5,6 +5,9 @@ window.addEventListener('load', () => {
 
     welcome.classList.add('welcome-in');
 
+    // setta il body non scrollabile per non rovinare l'animazione
+    document.body.style.overflow = 'hidden';
+
     setTimeout(() => {
         welcome.classList.replace('welcome-in', 'welcome-out');
         content.classList.add('content-in');
@@ -14,4 +17,9 @@ window.addEventListener('load', () => {
         welcome.classList.add('hidden');
         content.classList.remove('hidden');
     }, 2500);
+
+    // alla fine dell'animazione riabilita lo scrolling verticale
+    setTimeout(() => {
+        document.body.style.overflow = "";
+    }, 4500);
 });
