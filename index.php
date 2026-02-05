a5f417720655415f96cacaf3556942f12d5eb7e8
Rondosvalle <rubenrusso360@proton.me>
2026-02-05 16:54:36 +0100

    Added footer with css and fixed modal and navbar css

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <link rel="stylesheet" href="assets/css/modal.css">
     <link rel="stylesheet" href="assets/css/navbar.css">
     <link rel="stylesheet" href="assets/css/welcome.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
 </head>
 <body>
 
@@ -20,42 +21,44 @@
     <?php require_once 'includes/sign_in.php' ?>
     <?php require_once 'includes/navbar.php' ?>
 
-        <div class="welcome hidden">
-            <h1 id="animatedWelcome">Welcome to iDea</h1>
-        </div>
-        <div class="content hidden">
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-            <p>Ciao</p>
-        </div>
+    <div class="welcome hidden">
+        <h1 id="animatedWelcome">Welcome to iDea</h1>
+    </div>
+    <div class="content hidden">
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+        <p>Ciao</p>
+    </div>
 
+
+    <?php require_once "includes/footer.php" ?>
 
     <script src="assets/js/navbar.js"></script>
     <script src="assets/js/modal.js"></script>
