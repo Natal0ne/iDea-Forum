93f433dd81275cad21d05eaa7bbb72118f7ee09e
asica106 <a.sica106@studenti.unisa.it>
2026-02-12 20:38:49 +0100

    Added some css and other profile_settings logic

--- includes/profile_settings.php
+++ includes/profile_settings.php
@@ -6,21 +6,23 @@ session_start();
 
 $name = $_SESSION["username"] ?? "";
 $user = $_SESSION["user_id"] ?? null;
-//$avatar = $_SESSION["avatar_url"] ?? null;
+$avatar = $_SESSION["avatar_url"] ?? null;
 $conn = connect_db(); // connessione
 
 $current_bio = "";
-
+$current_signature = "";
 // recupero bio esistente
 if ($user) {
     $result = pg_query_params(
         $conn,
-         "SELECT bio FROM users WHERE id = $1",
+         "SELECT bio, signature FROM users WHERE id = $1",
         array($user)
     );
 
     if ($row = pg_fetch_assoc($result)) {
         $current_bio = $row["bio"] ?? "";
+        $current_signature = $row["signature"] ?? "";
+
     }
 }
 
@@ -28,11 +30,12 @@ if ($user) {
 if ($_SERVER["REQUEST_METHOD"] === "POST" && $user) {
 
     $bio = $_POST["bio"];
+    $signature = $_POST["signature"];
 
     pg_query_params(
         $conn,
-        "UPDATE users SET bio = $1 WHERE id = $2",
-        array($bio, $user)
+        "UPDATE users SET bio = $1, signature = $2 WHERE id = $3",
+        array($bio, $signature, $user)
     );
 
     $current_bio = $bio; // aggiorna subito
@@ -44,17 +47,18 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && $user) {
 ?>
 <html>
     <head>     
-<link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
     </head>
     <body>
     <h1 id="PfSettings">Profile Settings, welcome <?php echo htmlspecialchars($name); ?></h1>
 
-<form id="PfSettingsForm" method="POST" action="profile_settings.php">
-    <textarea name="bio" id="bio" maxlength="500" placeholder="Insert Bio">
-        <?php echo htmlspecialchars($current_bio); ?>
-    </textarea><br><br>
-
-    <button id="submitBtn" type="submit"> Save </button>
+<form id="PfSettingsForm" method="POST" action="profile_settings.php" enctype="multipart/form-data">
+    <textarea name="bio" rows="5" placeholder="Write a bio..."><?php echo htmlspecialchars($current_bio); ?></textarea>
+    <input type = "text"  name = "signature" id = "signature" placeholder="Enter a signature"        
+    value="<?php echo htmlspecialchars($current_signature); ?>"/> 
+    <button type="submit">Salva</button>
 </form>
 </body>
 </html>
