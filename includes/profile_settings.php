ec1f2eb998c09b04893cac6aecbd5cb0a882b512
Rondosvalle <rubenrusso360@proton.me>
2026-02-20 21:35:54 +0100

    Modified avatar in pf settings

--- includes/profile_settings.php
+++ includes/profile_settings.php
@@ -1,6 +1,6 @@
-<?php require_once 'profile_settings_data.php'; ?>
+<?php require_once "profile_settings_data.php"; ?>
 
-<div id="profileSettingsModal" class="modal <?php echo $profile_settings_modal_class?>">
+<div id="profileSettingsModal" class="modal <?php echo $profile_settings_modal_class; ?>">
 
  <div class="modal-overlay"></div>
 
@@ -14,60 +14,69 @@
         <div id='profileSettingsErrorMsg'>
             <?php if (!empty($profile_settings_error_message)): ?>
                 <div class="error-box">
-                    <p class="error"><?php echo htmlspecialchars($profile_settings_error_message); ?></p>
+                    <p class="error"><?php echo htmlspecialchars(
+                        $profile_settings_error_message,
+                    ); ?></p>
                 </div>
             <?php endif; ?>
         </div>
 
         <form action="includes/profile_settings_process.php" method="post" enctype="multipart/form-data">
-        <div class="avatar-container">
-            <label>Avatar</label>
-            <img src="<?php echo htmlspecialchars($_SESSION['user_avatar_url']); ?>" 
-            alt="Avatar" 
-            class="avatar" 
-            id="avatarPreview">
+            <div class="pf-avatar-container">
+                <label>Avatar</label>
+                <img src="<?php echo htmlspecialchars(
+                    $_SESSION["user_avatar_url"],
+                ); ?>"
+                alt="Avatar"
+                class="avatar"
+                id="avatarPreview">
 
-        <!-- input nascosto -->
-            <input type="file" 
-            name="avatar" 
-            id="avatarInput" 
-            accept="image/*" 
-            style="display: none;">
-        </div></br>
-        
-        <label for = "username" class="form-label">Username</label>
-        <input type = "text"  name = "username"  id = "username" class = "PFInput"
-        value = "<?php echo htmlspecialchars($_SESSION['username']); ?>" placeholder="Username"/>
+            <!-- input nascosto -->
+                <input type="file"
+                name="avatar"
+                id="avatarInput"
+                accept="image/*"
+                style="display: none;">
+            </div></br>
+            <label for = "username" class="form-label">Username</label>
+            <input type = "text"  name = "username"  id = "username" class = "PFInput"
+            value = "<?php echo htmlspecialchars(
+                $_SESSION["username"],
+            ); ?>" placeholder="Username"/>
 
-        <label for="bio" id="label-bio" class = "form-label">Bio</label>
-        <textarea name="bio" id = "bio" rows="5" placeholder="Write a bio..."><?php echo htmlspecialchars($_SESSION['user_bio']?? ''); ?></textarea>
+            <label for="bio" id="label-bio" class = "form-label">Bio</label>
+            <textarea name="bio" id = "bio" rows="5" placeholder="Write a bio..."><?php echo htmlspecialchars(
+                $_SESSION["user_bio"] ?? "",
+            ); ?></textarea>
 
-        <label for="signature" id="label-signature" class = "form-label">Signature</label>
-        <input type = "text"  name = "signature" id = "signature" class = "PFInput" placeholder="Enter a signature"
-        value="<?php echo htmlspecialchars($_SESSION['user_signature'] ?? ''); ?>"/>
+            <label for="signature" id="label-signature" class = "form-label">Signature</label>
+            <input type = "text"  name = "signature" id = "signature" class = "PFInput" placeholder="Enter a signature"
+            value="<?php echo htmlspecialchars(
+                $_SESSION["user_signature"] ?? "",
+            ); ?>"/>
 
-        <label class="form-label">Show Signature</label>
-            <span id="showSignature">
-                <label for="showSignature-yes" class="radio-option">
-                    <input type="radio" name="showSignature" id="showSignature-yes" value="Yes">
-                    <span>Yes</span>
-                </label>
+            <label class="form-label">Show Signature</label>
+                <span id="showSignature">
+                    <label for="showSignature-yes" class="radio-option">
+                        <input type="radio" name="showSignature" id="showSignature-yes" value="Yes">
+                        <span>Yes</span>
+                    </label>
 
-                <label for="showSignature-no" class="radio-option">
-                    <input type="radio" name="showSignature" id="showSignature-no" value="No">
-                    <span>No</span>
-                </label>
-            </span>
-        <label for="location">Where are you from?</label>
-        <input type = "text" name="location" id="location" placeholder="Insert your Country">
+                    <label for="showSignature-no" class="radio-option">
+                        <input type="radio" name="showSignature" id="showSignature-no" value="No">
+                        <span>No</span>
+                    </label>
+                </span>
+            <label for="location">Where are you from?</label>
+            <input type = "text" name="location" id="location" placeholder="Insert your Country">
 
-        <label for="website">Your site</label>
-        <input type = "text" name="website" id="website" placeholder="Insert your website">
+            <label for="website">Your site</label>
+            <input type = "text" name="website" id="website" placeholder="Insert your website">
 
-        <label for="language">Main Language</label>
-        <input type="text" name="language" id="language" placeholder="Insert main language">
+            <label for="language">Main Language</label>
+            <input type="text" name="language" id="language" placeholder="Insert main language">
 
-        <button type="submit" id = "savePfChanges" class = "btn-submit">Salva</button>
+            <button type="submit" id = "savePfChanges" class = "btn-submit">Salva</button>
         </form>
     </div>
 </div>
