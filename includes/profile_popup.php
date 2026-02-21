90f0d9516261e7edf673dcce765d7a4c89baad87
Rondosvalle <rubenrusso360@proton.me>
2026-02-21 17:35:57 +0100

    Added user location in user popup

--- includes/profile_popup.php
+++ includes/profile_popup.php
@@ -14,11 +14,13 @@
             <div class="profile-popup-info-item-label">Last Active: </div>
             <div class="profile-popup-info-item-label">Joined:</div>
             <div class="profile-popup-info-item-label">Reputation: </div>
+            <?php echo !$post['user_location'] ? '' : '<div class="profile-popup-info-item-label">Location: </div>' ?>
         </div>
         <div class="profile-popup-info-values">
             <div class="profile-popup-info-item-value"><?php echo date('j M Y', strtotime($post['user_last_active_at'])); ?></div>
             <div class="profile-popup-info-item-value"><?php echo date('F Y', strtotime($post['user_created_at'])); ?></div>
             <div class="profile-popup-info-item-value"><?php echo $post['user_reputation'] ?></div>
+            <?php echo !$post['user_location'] ? '' : '<div class="profile-popup-info-item-value">' . $post['user_location'] . '</div>' ?>
         </div>
     </div>
 </div>
